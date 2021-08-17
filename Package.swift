1f2ada74f0c70f45b368f5dc6c883cfd0aac9177
Clément Padovani <676023+clementpadovani@users.noreply.github.com>
2021-08-17 09:56:29 +0200

    Updated SPM definition

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "CombineCombineLatestArity",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13),
+                .macOS(.v10_15),
+                .tvOS(.v13),
+                .watchOS(.v6)],
     products: [
         .library(
             name: "CombineCombineLatestArity",
@@ -13,14 +16,10 @@ let package = Package(
             targets: ["CombineCombineLatestArity"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tcldr/Entwine.git", from: "0.9.1"),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", .upToNextMinor(from: "1.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CombineCombineLatestArity",
             dependencies: []),
